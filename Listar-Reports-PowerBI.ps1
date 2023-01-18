c2f7ca912568feb572315032ca9175f17b3145c3
JakyLindemberg <jaky.lindemberg@gmail.com>
2023-01-18 20:15:56 -0300

    Versão 1 - Listar Relatórios Power Bi

---
+++ Listar-Reports-PowerBI.ps1
@@ -0,0 +1,20 @@
+﻿Connect-PowerBIServiceAccount
+
+$Workspaces = @('Dashboards Desk Manager','Dashboards Service Now','Geral')
+
+
+clear
+
+foreach($workspace in $Workspaces) {
+
+$ws = Get-PowerBIWorkspace -Name $workspace
+$relatorios = Get-PowerBIReport -Workspace $ws
+
+foreach($relatorio in $relatorios) {
+ 
+ $arquivo = $relatorio.name + ";" + $Workspace
+  
+ Write-Output $arquivo
+
+}
+}
